2d8eff19b7e7ee05c6a53ae04ea5c3eb6bde8aad
INBGNL0350\Pooja CK <pooja.ck@asmltd.com>
2022-12-16 12:45:23 +0530

    friends series

--- learning.py
+++ learning.py
@@ -2,4 +2,10 @@ a = input("hello did u started to learn git?")
 if a == "yes":
     print("good job!")
 else:
-    print("then ur lazy goose!")
+    print("then ur lazy!")
+b = input("ok atleast do u watch friends series??")
+if b == "yes":
+    print("then ur awesome!!")
+else:
+    print("ur useless!")
+
